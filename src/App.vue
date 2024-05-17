430744209e230719f8cc43bac673c735899e4831
alexwertand <alexandr_works@ukr.net>
2024-05-17 16:23:33 +0300

    added webpack config files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-    import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<h1>abz agency 1</h1>
 </template>
 
+<script>
+	import { defineComponent } from 'vue'; 
+
+	export default defineComponent({
+		name: 'App'
+	});
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+	h1 {
+		width: 100%;
+		text-align: center;
+		margin-top: 10px;
+	}
+
+	img {
+		display: block;
+		margin: auto;
+	}
 </style>
